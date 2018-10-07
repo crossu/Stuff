aee507b1325df7173299841920fcf80eee532584
Chamny <michal.chamny@gmail.com>
2018-10-07 19:32:14 +0200

    Completed MVC Training on MS Site

---
+++ MVC Practice/MVC Practice/Data/MVC_PracticeContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVC_Practice.Models
+{
+    public class MVC_PracticeContext : DbContext
+    {
+        public MVC_PracticeContext (DbContextOptions<MVC_PracticeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVC_Practice.Models.Movie> Movie { get; set; }
+    }
+}
